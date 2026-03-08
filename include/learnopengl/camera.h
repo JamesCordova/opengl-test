b643eeacdec802adc873e6ac9dcb19094d801c19
james <jcordovas@gmail.com>
2026-03-08 16:53:32 -0500

    Class Camera implemented

---
+++ include/learnopengl/camera.h
@@ -0,0 +1,63 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <glm/glm.hpp>
+
+enum Camera_Movement
+{
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+};
+
+// Default camera values
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 2.5f;
+const float SENSITIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
+class Camera
+{
+public:
+    // Camera Attributes
+    glm::vec3 Position;
+    glm::vec3 Front;
+    glm::vec3 Up;
+    glm::vec3 Right;
+    glm::vec3 WorldUp;
+    // Euler Angles
+    float Yaw;
+    float Pitch;
+    // Camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+    // Constructor with vectors
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
+           glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+           float yaw = YAW,
+           float pitch = PITCH);
+    // Constructor with scalar values
+    Camera(float posX, float posY, float posZ,
+           float upX, float upY, float upZ,
+           float yaw, float pitch);
+
+    glm::mat4 GetViewMatrix();
+
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessMouseMovement(float xoffset, float yoffset, bool constrainPitch = true);
+    void ProcessMouseScroll(float yoffset);
+    
+    void ResetCamera();
+
+private:
+    // Calculates the front vector from the Camera's Euler Angles
+    void updateCameraVectors();
+};
+
+#endif
