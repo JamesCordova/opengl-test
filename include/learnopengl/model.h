99fc09e10fa5f7ee94a3ef76f07c732ce7172323
james <jcordovas@gmail.com>
2026-03-11 08:34:23 -0500

    Model class implemented alongside CMakelists configured

---
+++ include/learnopengl/model.h
@@ -0,0 +1,32 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+#include "learnopengl/mesh.h"
+
+// #include <assimp/scene.h>
+struct aiNode;
+struct aiMesh;
+struct aiMaterial;
+struct aiScene;
+enum aiTextureType;
+
+unsigned int TextureFromFile(const char *path, const std::string &directory);
+
+class Model
+{
+public:
+    Model(char *path);
+    void Draw(Shader &shader);
+private:
+    // model data
+    std::vector<Mesh> meshes;
+    std::string directory;
+
+    void loadModel(std::string path);
+    void processNode(aiNode *node, const aiScene *scene);
+    Mesh processMesh(aiMesh *mesh, const aiScene *scene);
+    std::vector<Texture> loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName);
+
+};
+
+#endif
