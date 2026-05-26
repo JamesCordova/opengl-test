9d584b3c403ffb668bbdc9b14e2910d177f5af15
JamesCordova <jcordovas@unsa.edu.pe>
2026-05-26 11:37:12 -0500

    feat(shader): Add setVec4 method to set a vec4 uniform in shaders

--- src/learnopengl/shader.cpp
+++ src/learnopengl/shader.cpp
@@ -133,6 +133,12 @@ void Shader::setVec3(const std::string &name, const glm::vec3 &value) const
 {
     glUniform3f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z);
 }
+
+void Shader::setVec4(const std::string &name, const glm::vec4 &value) const
+{
+    glUniform4f(glGetUniformLocation(ID, name.c_str()), value.x, value.y, value.z, value.w);
+}
+
 void Shader::setMat4(const std::string &name, const glm::mat4 &mat) const
 {
     glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
